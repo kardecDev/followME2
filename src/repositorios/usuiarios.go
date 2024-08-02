5c96f464d88d0a8777627fbffce685e0754e9efd
Allan Kardec Neves Alves <kardec.dev@gmail.com>
2024-08-02 10:17:27 -0300

    Feat: Correçao Dockerfile retirando cp dos go.mod e go.sum, repositorio usuario, corrigido commando inserte adicionando returning id

--- src/repositorios/usuiarios.go
+++ src/repositorios/usuiarios.go
@@ -19,16 +19,15 @@ func (repositorio usuarios) Criar(usuario modelos.Usuarios) (uint64, error) {
 	var ultimoIdInserido uint64
 
 	statement, erro := repositorio.db.Prepare(
-		"insert into usuarios (nome, nick, email, senha) values($1,$2,$3,$4)",
+		"insert into usuarios (nome, nick, email, senha) values($1,$2,$3,$4) RETURNING id",
 	)
-
 	if erro != nil {
 		return 0, erro
 	}
 
 	defer statement.Close()
 
-	erro = statement.QueryRow(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha).Scan(ultimoIdInserido)
+	erro = statement.QueryRow(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha).Scan(&ultimoIdInserido)
 	if erro != nil {
 		return 0, erro
 	}
