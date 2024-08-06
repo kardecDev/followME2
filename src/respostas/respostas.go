35ae58d15274122b69552821b708a9d9d31bb68c
Allan Kardec Neves Alves <kardec.dev@gmail.com>
2024-08-06 17:47:29 -0300

    Feat: Criação do metodo de alteração de usuario por id, exceto campo senha

--- src/respostas/respostas.go
+++ src/respostas/respostas.go
@@ -8,9 +8,13 @@ import (
 
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 	w.WriteHeader(statusCode)
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+
+	if dados != nil {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
+
 }
 
 func Erro(w http.ResponseWriter, statusCode int, erro error) {
